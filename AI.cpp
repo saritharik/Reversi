23fa0ffd24a70a77ea15242ce5cbe780e0fd9e86
Sarit Harik <saritharik@gmail.com>
2017-11-26 01:20:14 +0200

    fixed bugs.

---
+++ AI.cpp
@@ -0,0 +1,63 @@
+//
+// Created by sarit on 25/11/17.
+//
+
+#include "AI.h"
+
+AI::AI(GameLogic* g, Board* b): board(b), game(g) {
+    points = 0;
+}
+
+char AI::getDisk() {
+    return 'O';
+}
+
+Point AI::chooseSquare() {
+    int min = 100;
+    Point choice(0,0);
+    vector<Point> vec = game->findPoints(getDisk());
+    vector<Point>::iterator iter;
+    for (iter = vec.begin(); iter != vec.end(); iter++) {
+        board->setSquare(iter.base()->getX(), iter.base()->getY(), getDisk());
+        vector<Point> pointsToFlip = game->checking(iter.base()->getX(), iter.base()->getY(), getDisk());
+        vector<Point>::iterator it;
+        for (it = pointsToFlip.begin(); it != pointsToFlip.end(); it++) {
+            game->replaceDisk(it.base()->getX(), it.base()->getY());
+        }
+
+        int max = maxOption();
+        if (max < min) {
+            min = max;
+            choice = Point(iter.base()->getX(), iter.base()->getY());
+        }
+        //flip to the original state
+        for (it = pointsToFlip.begin(); it != pointsToFlip.end(); it++) {
+            game->replaceDisk(it.base()->getX(), it.base()->getY());
+        }
+        board->setSquare(iter.base()->getX(), iter.base()->getY(), ' ');
+    }
+    return choice;
+}
+
+int AI::maxOption() {
+    int max = 0;
+    int boardPoints = 0;
+    int points;
+    vector<Point> vec = game->findPoints('X');
+    vector<Point>::iterator iter;
+    for (iter = vec.begin(); iter != vec.end(); iter++) {
+        points = game->checking(iter.base()->getX(), iter.base()->getY(), 'X').size();
+        boardPoints = 1 + (2 * points) + game->getPointsByPlayer('X') - getPoint();
+        if (boardPoints > max) {
+            max = boardPoints;
+        }
+    }
+    return boardPoints;
+}
+void AI::setPoint(int newPoints) {
+    points += newPoints;
+}
+
+int AI::getPoint() {
+    return points;
+}
