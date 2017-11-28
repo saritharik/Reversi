a0e82560fd55d4183f8c956640f96dece1448c4a
Sarit Harik <saritharik@gmail.com>
2017-11-28 17:24:14 +0200

    fixed bugs.

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@
 //
 #include <iostream>
 #include "GameLogic.h"
+#include "AI.h"
 
 using namespace std;
 
 int main() {
     Human p1('X');
-    Human p2('O');
+    //Human p2('O');
+    //AI p2(&game, &bC);
     BoardConsole bC(8, 8);
-    GameLogic game(&p1, &p2, &bC);
-    game.playGame();
+    //GameLogic game(&p1, &p2, &bC);
+
+    //game.playGame();
     return 0;
 }
